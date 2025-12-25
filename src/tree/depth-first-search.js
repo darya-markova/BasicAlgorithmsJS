b1cf0befbdf2fa0dbd01ec1e1968611f8b6f852f
darya-markova <darya.towarddreams@gmail.com>
2025-12-25 22:59:47 +0300

    depth first search algorithm

---
+++ src/tree/depth-first-search.js
@@ -0,0 +1,37 @@
+function initCallbacks(callbacks = {}) {
+  const initiatedCallbacks = {};
+  const stubCallback = () => {};
+  const defaultAllowTraversalCallback = () => true;
+
+ 
+  initiatedCallbacks.allowTraversal = callbacks.allowTraversal || defaultAllowTraversalCallback;
+  initiatedCallbacks.enterNode = callbacks.enterNode || stubCallback;
+  initiatedCallbacks.leaveNode = callbacks.leaveNode || stubCallback;
+
+  return initiatedCallbacks;
+}
+
+export function depthFirstSearchRecursive(node, callbacks) {
+  callbacks.enterNode(node);
+
+  if (node.left && callbacks.allowTraversal(node, node.left)) {
+    depthFirstSearchRecursive(node.left, callbacks);
+  }
+
+  if (node.right && callbacks.allowTraversal(node, node.right)) {
+    depthFirstSearchRecursive(node.right, callbacks);
+  }
+
+  callbacks.leaveNode(node);
+}
+
+/**
+ * @param {BinaryTreeNode} rootNode - корневой узел
+ * @param {TraversalCallbacks} [callbacks] - система коллбэков
+ */
+export default function depthFirstSearch(rootNode, callbacks) {
+  const processedCallbacks = initCallbacks(callbacks);
+
+  depthFirstSearchRecursive(rootNode, processedCallbacks);
+}
+
