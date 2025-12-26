46e6e25ce0a2d636334bce55d6950a9b4e016990
darya-markova <darya.towarddreams@gmail.com>
2025-12-26 12:36:13 +0300

    breadth first search algorithms

--- src/tree/breadth-first-search.js
+++ src/tree/breadth-first-search.js
@@ -1,1 +1,29 @@
-// обход дерева в ширину
+// обход в ширину
+
+const initiatedCallback = {
+  enterNode: function(node) {},
+  leaveNode: function(node) {
+
+  }
+};
+
+export function breadthFirstSearch(rootNode, callbacks) {
+  const nodeQueue = []; 
+
+  nodeQueue.push(rootNode);
+
+  while(nodeQueue.length) {
+    const currentNode = nodeQueue.shift();
+
+    // обрабатываем вот этот узел callbacks.enterNode(currentNode);
+    if (currentNode.left) {
+      nodeQueue.push(currentNode.left);
+    }
+
+    if (currentNode.right) {
+      nodeQueue.push(currentNode.right);
+    }
+
+    // callbacks.leaveNode(currentNode)
+  }
+}
